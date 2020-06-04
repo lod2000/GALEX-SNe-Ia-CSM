211fc7fa791e4abd838a3ba8017df5941ecff979
Liam Dubay <liam.dubay@gmail.com>
2020-06-04 11:03:28 -0700

    update classify

--- classify.py
+++ classify.py
@@ -47,17 +47,11 @@ if __name__ == '__main__':
 
     # Read clean OSC csv
     osc = utils.import_osc(Path('ref/OSC-pre2014-expt-clean.csv'))
-
-    #fits_dir = Path('/mnt/d/GALEX_SNeIa_REU/fits')
-    #fits_dir = Path('/mnt/exthdd/GALEX_SNeIa_REU/fits')
+    
     fits_dir = args.fits_dir
     fits_files = [f for f in fits_dir.glob('**/*.fits.gz')]
 
-    #categories = []
-    '''
-    for fits_file in tqdm(fits_files):
-        categories.append(classify_fits(fits_file))
-    '''
+    # multiprocess classification
     with mp.Pool(4) as pool:
         categories = list(tqdm(pool.imap(classify_fits, fits_files), 
                 total=len(fits_files)))

20665bb0543e8c3baf6f08856d38d98405fdbbee
Liam Dubay <liam.dubay@gmail.com>
2020-06-09 15:27:22 -0700

    updated filter plot

--- filters.py
+++ filters.py
@@ -2,15 +2,22 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from pathlib import Path
+import os
 
 fig, ax = plt.subplots()
 
-filter_dir = Path('filters/')
-filters = [f for f in filter_dir.glob('**/*.dat')]
+os.chdir('filters')
+filters = sorted(os.listdir(os.getcwd()))
 
 for f in filters:
-    name = f.name.split('.')[:-1]
+    inst = f.split('.')[0].replace('_',' ')
+    name = f.split('.')[-2]
     array = np.loadtxt(f, delimiter=' ')
-    ax.plot(array[:,0], array[:,1])
+    ax.plot(array[:,0], array[:,1], label=' '.join([inst, name]))
+
+ax.set_xlabel('Wavelength (Å)')
+ax.set_xlim((1000, 4000))
+ax.set_ylabel('Effective area (cm^2)')
+ax.legend()
 
 plt.show()

25c55b2c82a4a9c7e92bddde3c3a46599e801a80
Liam Dubay <liam.dubay@gmail.com>
2020-06-09 17:55:37 -0700

    updated filter response curves

--- filters.py
+++ filters.py
@@ -13,11 +13,31 @@ for f in filters:
     inst = f.split('.')[0].replace('_',' ')
     name = f.split('.')[-2]
     array = np.loadtxt(f, delimiter=' ')
-    ax.plot(array[:,0], array[:,1], label=' '.join([inst, name]))
+    freq = array[:,0]
+    y = array[:,1]
+
+    # convert effective area to throughput
+    if inst == 'GALEX':
+        y = y/(np.pi * 25**2) # objective diameter: 50 cm
+    if inst == 'Swift UVOT':
+        y = y/(np.pi * 15**2) # objective diameter: 30 cm
+
+    # make GALEX curves more visible
+    alpha = 1 if inst=='GALEX' else 0.8
+    if 'Swift' in inst:
+        style = '--'
+    elif 'HST' in inst:
+        style = ':'
+    else:
+        style = '-'
+
+    ax.plot(freq, y, label=' '.join([inst, name]), linestyle=style, alpha=alpha)
 
 ax.set_xlabel('Wavelength (Å)')
-ax.set_xlim((1000, 4000))
-ax.set_ylabel('Effective area (cm^2)')
+ax.set_xlim((1000, 3500))
+#ax.set_yscale('log')
+#ax.set_ylim((5e-3,0.14))
+ax.set_ylabel('Transmission')
 ax.legend()
 
 plt.show()

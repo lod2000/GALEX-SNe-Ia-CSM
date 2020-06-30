b5dad168d501584abf3284e43b3e9fa914278a82
Liam Dubay <liam.dubay@gmail.com>
2020-06-30 13:27:23 -0700

    moved table generator to different script

--- scrape_ned.py
+++ scrape_ned.py
@@ -26,11 +26,6 @@ QUERY_RADIUS = 5. # arcmin
 BLOCK_SIZE = 10
 SN_INFO_FILE = Path('ref/sn_info.csv')
 NED_RESULTS_FILE = Path('ref/ned.csv')
-BIB_FILE = Path('tex/table_references.bib')
-CAT_FILE = Path('tex/catalog_codes.txt')
-LATEX_TABLE_TEMPLATE = Path('tex/deluxetable_template.tex')
-LATEX_TABLE_FILE = Path('tex/table.tex')
-SHORT_TABLE_FILE = Path('tex/short_table.tex')
 
 
 def main():
@@ -67,8 +62,7 @@ def main():
     sn_info = combine_sn_info(ned, sn_info)
     utils.output_csv(sn_info, SN_INFO_FILE)
 
-    #plot_redshifts(ned)
-    to_latex(sn_info)
+    # plot_redshifts(ned)
 
 
 def get_sn(sn, sn_info, osc, verb=0):
@@ -280,23 +274,6 @@ def plot_redshifts(ned, bin_width=0.025):
     plt.close()
 
 
-def get_catalogs(ned):
-    """
-    Get catalog refcodes (denoted by trailing ':' in NED)
-    """
-
-    ned.dropna(subset=['z'], inplace=True)
-    ref_cols = ['posn_ref', 'z_ref', 'morp_ref']
-    catalogs = []
-    for col in ref_cols:
-        catalogs += list(ned[ned['posn_ref'].str.contains(':')]['posn_ref'])
-    catalogs = list(dict.fromkeys(catalogs))
-    catalog_str = '\n'.join(catalogs)
-    with open(CAT_FILE, 'w') as file:
-        file.write(catalog_str)
-    return catalogs
-
-
 def combine_sn_info(ned, sn_info):
     """
     Combines SNe from NED and sn_info, selecting only those with before+after
@@ -322,85 +299,6 @@ def combine_sn_info(ned, sn_info):
     sn_info = sn_info.loc[:,~sn_info.columns.duplicated()]
 
     return sn_info
-
-
-def to_latex(sn_info):
-    """
-    Outputs NED scrape output and important FITS info to LaTeX table
-    """
-
-    # Format coordinates, redshifts & distances
-    sn_info['galex_coord'] = sn_info[['galex_ra', 'galex_dec']].agg(', '.join, axis=1)
-    sn_info['z_str'] = sn_info['z'].round(6).astype('str').replace('0+$','',regex=True)
-    sn_info['h_dist_str'] = sn_info['h_dist'].round(0).astype(int)
-    # Add notes
-    sn_info['notes'] = sn_info[['z_flag']].astype(str).replace('nan', 'N/A').agg('; '.join, axis=1)
-    # Concat references
-    sn_info['refs'] = sn_info[['z_ref', 'morph_ref']].astype('str').agg(';'.join, axis=1)
-
-    # Get BibTeX entries and write bibfile
-    overwrite = True
-    if BIB_FILE.is_file():
-        over_in = input('Previous bibliography detected. Overwrite? [y/N] ')
-        overwrite = (over_in == 'y')
-
-    if overwrite:
-        print('Pulling BibTeX entries from ADS...')
-        refs = list(sn_info['posn_ref']) + list(sn_info['z_ref']) + list(sn_info['morph_ref'])
-        refs = list(dict.fromkeys(refs)) # remove duplicates
-        bibcodes = {'bibcode':refs}
-        with open('ads_token', 'r') as file:
-            token = file.readline()
-        ads_bibtex_url = 'https://api.adsabs.harvard.edu/v1/export/bibtex'
-        r = requests.post(ads_bibtex_url, headers={'Authorization': 'Bearer ' + token}, data=bibcodes)
-        bibtex = r.json()['export'].replace('A&A', 'AandA') # replace pesky ampersands
-        with open(BIB_FILE, 'w') as file:
-            file.write(bibtex)
-
-    print('Writing to LaTeX table...')
-    # Format reference bibcodes
-    formatters = {'refs':table_ref}
-    columns = ['name', 'disc_date', 'galex_coord', 'epochs_total', 
-            'delta_t_first', 'delta_t_last', 'delta_t_next', 'z_str', 
-            'h_dist_str', 'a_v', 'morph', 'refs']
-    # Generate table
-    sn_info.reset_index(inplace=True)
-    latex_table = sn_info.to_latex(na_rep='N/A', index=False, escape=False,
-        columns=columns, formatters=formatters
-    )
-    # Replace table header and footer with template
-    # Edit this file if you need to change the number of columns or description
-    with open(LATEX_TABLE_TEMPLATE, 'r') as file:
-        dt_file = file.read()
-        header = dt_file.split('===')[0]
-        footer = dt_file.split('===')[1]
-    latex_table = header + '\n'.join(latex_table.split('\n')[4:-3]) + footer
-    # Write table
-    with open(LATEX_TABLE_FILE, 'w') as file:
-        file.write(latex_table)
-
-    # Generate short table
-    short = sn_info.iloc[0:20]
-    short_table = short.to_latex(na_rep='N/A', index=False, escape=False,
-        columns=columns, formatters=formatters
-    )
-    short_table = header + '\n'.join(short_table.split('\n')[4:-3]) + footer
-    with open(SHORT_TABLE_FILE, 'w') as file:
-        file.write(short_table)
-
-    # Catalog bibcodes (NED has a weird format)
-    get_catalogs(sn_info)
-
-
-def table_ref(bibcodes):
-    """
-    Formats reference bibcodes for LaTeX table
-    Input:
-        bibcodes (str): list of reference codes joined with ';'
-    """
-    bibcodes = bibcodes.replace(';nan', '').split(';')
-    bibcodes = list(dict.fromkeys(bibcodes)) # remove duplicates
-    return '\citet{%s}' % ','.join([b.replace('A&A', 'AandA') for b in bibcodes])
 
 
 if __name__ == '__main__':

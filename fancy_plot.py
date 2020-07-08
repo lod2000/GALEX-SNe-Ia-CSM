a0f8b1fae56d3d79a970434095ac7c553c904901
Liam Dubay <liam.dubay@gmail.com>
2020-07-08 10:22:48 -0700

    tweaked before points

--- fancy_plot.py
+++ fancy_plot.py
@@ -14,33 +14,31 @@ def main():
     last_epoch = sn_info.loc[sn, 'delta_t_last']
 
     data = [full_import(sn, band, sn_info) for band in bands]
+    colors = {'FUV': 'm', 'NUV': 'b'}
 
     fig, ax = plt.subplots()
     fig.set_tight_layout(True)
-
-    colors = {'FUV': 'm', 'NUV': 'b'}
 
     # Get largest flux exponent
     fluxes = np.concatenate([lc['flux_bgsub'].to_numpy() for lc in data])
     flux_exp = int(np.log10(np.max(fluxes))) - 1
     yscale = 1 / (10**flux_exp)
 
+    # Plot points after discovery
     for lc, band in zip(data, bands):
-
         color = colors[band]
+        after = lc[(lc['t_delta'] > DT_MIN) & (lc['t_delta'] < dt_max)]
 
         # Systematics
         bg, bg_err, sys_err = get_background(lc, 'flux')
 
-        before = lc[lc['t_delta'] <= DT_MIN]
-        after = lc[(lc['t_delta'] > DT_MIN) & (lc['t_delta'] < dt_max)]
-
         # Plot background average of epochs before discovery
+        bg_sigma = 3
         plt.axhline(bg * yscale, 0, 1, color=color, alpha=0.5, linestyle='--', 
-            linewidth=1, label=band+' host background')
-        plt.axhspan(ymin=(bg - 2 * bg_err) * yscale, 
-                ymax=(bg + 2 * bg_err) * yscale, 
-                color=color, alpha=0.2, label=band+' host 2σ')
+                linewidth=1, label=band+' host background')
+        plt.axhspan(ymin=(bg - bg_sigma * bg_err) * yscale, 
+                ymax=(bg + bg_sigma * bg_err) * yscale, 
+                color=color, alpha=0.2, label=band+' host %sσ' % bg_sigma)
 
         # Plot fluxes
         ax.errorbar(after['t_delta'], after['flux_bgsub'] * yscale, 
@@ -49,11 +47,13 @@ def main():
                 elinewidth=1, c=color, label=band+' flux'
         )
 
-        # Plot all points before discovery on a compressed scale
-        dt = (min((last_epoch, dt_max)) - nearest_epoch) / 20
-        before_t = [nearest_epoch - dt] * len(before.index)
+    # Indicate points before discovery
+    xmin = ax.get_xlim()[0]
+    for lc, band in zip(data, bands):
+        before = lc[lc['t_delta'] <= DT_MIN]
+        before_t = [xmin] * len(before.index)
         ax.scatter(before_t, before['flux_bgsub'] * yscale, marker='<', s=5,
-                c=color)
+                c=colors[band])
 
     # Configure plot
     ax.set_xlabel('Time since discovery [days]')
@@ -68,7 +68,7 @@ def main():
     luminosity_ax.set_ylim(ylim_luminosity / (10**luminosity_exp))
     luminosity_ax.set_ylabel('Luminosity [$10^{%s}$ erg s$^{-1}$ Å$^{-1}$]' % luminosity_exp, 
             rotation=270, labelpad=24)
-    
+
     plt.show()
 
 

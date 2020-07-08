460cca773fa59b448a03d816d930a2f965059270
Liam Dubay <liam.dubay@gmail.com>
2020-07-08 11:46:12 -0700

    improved fancy plot legend

--- fancy_plot.py
+++ fancy_plot.py
@@ -1,11 +1,13 @@
 import matplotlib
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import matplotlib.lines as mlines
 from lc_utils import *
 
 
 def main():
     sn = 'ESSENCEn278'
-    dt_max = 1000
+    dt_max = 10000
     bands = ['FUV', 'NUV']
 
     sn_info = pd.read_csv('ref/sn_info.csv', index_col='name')
@@ -16,7 +18,7 @@ def main():
     data = [full_import(sn, band, sn_info) for band in bands]
     colors = {'FUV': 'm', 'NUV': 'b'}
 
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(8, 5))
     fig.set_tight_layout(True)
 
     # Get largest flux exponent
@@ -34,11 +36,12 @@ def main():
 
         # Plot background average of epochs before discovery
         bg_sigma = 3
+        bg_alpha = 0.2
         plt.axhline(bg * yscale, 0, 1, color=color, alpha=0.5, linestyle='--', 
-                linewidth=1, label=band+' host background')
+                linewidth=1)
         plt.axhspan(ymin=(bg - bg_sigma * bg_err) * yscale, 
                 ymax=(bg + bg_sigma * bg_err) * yscale, 
-                color=color, alpha=0.2, label=band+' host %sσ' % bg_sigma)
+                color=color, alpha=bg_alpha)
 
         # Plot fluxes
         ax.errorbar(after['t_delta'], after['flux_bgsub'] * yscale, 
@@ -59,7 +62,19 @@ def main():
     ax.set_xlabel('Time since discovery [days]')
     ax.set_ylabel('Flux [$10^{%s}$ erg s$^{-1}$ Å$^{-1}$ cm$^{-2}$]' % flux_exp)
     ylim_flux = np.array(ax.get_ylim()) * 10**flux_exp
-    plt.legend()
+
+    # Add legend
+    handles, labels = ax.get_legend_handles_labels()
+    bg_line = mlines.Line2D([], [], color='k', linestyle='--', alpha=0.5,
+            label='host mean', linewidth=1)
+    bg_patch = mpatches.Patch(color='k', alpha=bg_alpha, label='host %sσ' % bg_sigma)
+    bg_point = mlines.Line2D([], [], color='k', linestyle='none', marker='<',
+            label='host flux')
+    plt.legend(handles=[bg_patch, bg_line, bg_point] + handles, ncol=5, 
+            loc='lower left', mode='expand', handletextpad=0.5, handlelength=1.2,
+            bbox_to_anchor=(0, 1.02, 1., 0), borderaxespad=0)
+    # plt.gca().add_artist(first_legend)
+    # plt.legend(loc='best')
 
     # Twin axis with absolute luminosity
     luminosity_ax = ax.twinx()
